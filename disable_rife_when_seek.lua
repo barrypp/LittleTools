e73975b4e146c8e857a650c17e66788413986c99
Barrypp.zzx <4166765+barrypp@users.noreply.github.com>
2023-01-10 04:56:28 +0800

    Add files via upload

--- disable_rife_when_seek.lua
+++ disable_rife_when_seek.lua
@@ -1,4 +1,4 @@
-local delay = 2
+local delay = 3
 local timer_restore = nil
 local p = {
     last_time_pos = 0,
@@ -16,9 +16,19 @@ function kill_and_add_timeout(timer,...)
     return mp.add_timeout(...)
 end
 
+-- function restore() --pause后再设置vf的方案导致视频暂停播放长达1.5~2s
+--     print_time("restore")
+--     p.pause = mp.get_property_native("pause")
+--     mp.set_property_bool("pause", true)
+--     p.last_time_pos = mp.get_property_native("time-pos") -- pause后会产生一个额外的seek事件
+--     mp.set_property("vf", p.vf)
+--     mp.osd_message("restore vf, vf=" .. p.vf)
+--     mp.observe_property("estimated-vf-fps", "native", on_estimated_vf_fps)
+-- end
+
 function restore()
     print_time("restore")
-    mp.set_property("vf", p.vf)
+    mp.set_property("vf", p.vf) --导致丢帧1s左右
     mp.osd_message("restore vf, vf=" .. p.vf)
 end
 
@@ -33,8 +43,24 @@ function on_seek()
         p.vf = mp.get_property("vf")
     end
     mp.set_property("vf", "")
-    mp.osd_message("temporarily disable vf")
+    mp.osd_message("temporarily disable vf", delay)
     timer_restore = kill_and_add_timeout(timer_restore, delay, restore)
 end
 
+-- function on_estimated_vf_fps(_,value) --使用estimated_vf_fps来判断vf是否准备完毕以避免跳帧
+--     print_time("on_estimated_vf_fps",value)
+--     if (value == nil) then
+--         return
+--     end
+--     mp.unobserve_property(on_estimated_vf_fps)
+--     p.time_pos_of_restore_pause = nil
+--     mp.set_property_bool("pause", p.pause)
+-- end
+
+-- function on_time_pos(_,value)
+--     print_time("on_time_pos",value)
+-- end
+-- mp.observe_property("time-pos", "native", on_time_pos)
+
 mp.register_event("seek", on_seek)
+
