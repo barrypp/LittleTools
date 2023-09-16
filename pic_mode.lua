1c0fc4025e1040529b48a675af4aa858a6082d20
Barrypp.zzx <4166765+barrypp@users.noreply.github.com>
2023-09-16 14:45:20 +0000

    add set for img ext

--- pic_mode.lua
+++ pic_mode.lua
@@ -2,6 +2,7 @@ local os_time_start = os.time()
 local fit_to_width = false
 local last_pic_is_prev = true
 local file_load_done = false
+local is_pic = false
 local l = {
     zoom = 0,
     pan_y_max = 0,
@@ -10,6 +11,26 @@ local l = {
 
 function print_time(...)
     print("+" .. os.time() - os_time_start .. "s", ...)
+end
+
+function Set (t) -- from autoload.lua
+    local set = {}
+    for _, v in pairs(t) do set[v] = true end
+    return set
+end
+
+ext_img = Set { -- from autoload.lua with modify
+    'avif', 'bmp', 'j2k', 'jp2', 'jpeg', 'jpg', 'jxl', 'png',
+    'svg', 'tga', 'tif', 'tiff', 'webp', 'psd'
+}
+
+function get_extension(path) -- from autoload.lua
+    match = string.match(path, "%.([^%.]+)$" )
+    if match == nil then
+        return "nomatch"
+    else
+        return match
+    end
 end
 
 function calc_pan_and_zoom()
@@ -39,7 +60,7 @@ function do_fit_to_width(f)
     end
 end
 
-function on_my_WHEEL_UP()
+function on_WHEEL_UP()
     if (not file_load_done) then
         return
     end
@@ -59,7 +80,7 @@ function on_my_WHEEL_UP()
     end
 end
 
-function on_my_WHEEL_DOWN()
+function on_WHEEL_DOWN()
     if (not file_load_done) then --在load done之前可能有多余操作
         return
     end    
@@ -79,7 +100,7 @@ function on_my_WHEEL_DOWN()
     end    
 end
 
-function on_my_MBTN_LEFT_DBL()
+function on_MBTN_LEFT_DBL()
     if (fit_to_width) then
         do_fit_to_width(false)
     else
@@ -89,31 +110,30 @@ end
 
 function on_start_file()
     file_load_done = false
-    name = mp.get_property_native("filename")
-    if (name:match("%.png$") or name:match("%.jpg$")) then
-        print_time("pic_mode on")
+    ext = get_extension(mp.get_property_native("filename"))
+    if (ext_img[string.lower(ext)] ~= nil) then
+        is_pic = true
         mp.set_property_bool("pause", true)
-        mp.add_forced_key_binding("WHEEL_UP","my_WHEEL_UP",on_my_WHEEL_UP)
-        mp.add_forced_key_binding("WHEEL_DOWN","my_WHEEL_DOWN",on_my_WHEEL_DOWN)
-        mp.add_forced_key_binding("MBTN_LEFT_DBL","my_MBTN_LEFT_DBL",on_my_MBTN_LEFT_DBL)
+        mp.add_forced_key_binding("WHEEL_UP","WHEEL_UP",on_WHEEL_UP)
+        mp.add_forced_key_binding("WHEEL_DOWN","WHEEL_DOWN",on_WHEEL_DOWN)
+        mp.add_forced_key_binding("MBTN_LEFT_DBL","MBTN_LEFT_DBL",on_MBTN_LEFT_DBL)
     elseif (name:match("%.gif$")) then
+        is_pic = false
         mp.set_property_bool("pause", false)
-        mp.add_forced_key_binding("WHEEL_UP","seek_1s_prev",function () mp.command("seek -1 exact") end)
-        mp.add_forced_key_binding("WHEEL_DOWN","seek_1s_next",function () mp.command("seek 1 exact") end)
+        mp.add_forced_key_binding("WHEEL_UP","WHEEL_UP",function () mp.command("seek -1 exact") end)
+        mp.add_forced_key_binding("WHEEL_DOWN","WHEEL_DOWN",function () mp.command("seek 1 exact") end)
     else
-        print_time("pic_mode off")
+        is_pic = false
         mp.set_property_bool("pause", false)
-        mp.remove_key_binding("my_WHEEL_UP")
-        mp.remove_key_binding("my_WHEEL_DOWN")
-        mp.remove_key_binding("my_MBTN_LEFT_DBL")        
-        mp.remove_key_binding("seek_1s_next")
-        mp.remove_key_binding("seek_1s_prev")
+        mp.remove_key_binding("WHEEL_UP")
+        mp.remove_key_binding("WHEEL_DOWN")
+        mp.remove_key_binding("MBTN_LEFT_DBL")
     end
 end
 
 function on_file_loaded()
     name = mp.get_property_native("filename")
-    if (name:match("%.png$") or name:match("%.jpg$")) then
+    if (is_pic) then
         do_fit_to_width(fit_to_width)
     else
         do_fit_to_width(false)
